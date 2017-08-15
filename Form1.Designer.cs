907ea2be198b78a34de4aa8f8815723083575734
shareyin <shareyin@qq.com>
2017-08-15 15:00:09 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Findpic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridViewout = new System.Windows.Forms.DataGridView();
+            this.JCartype = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.forcetime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewout)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "输入时间查找图片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(181, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "导入Excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(355, 275);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(367, 163);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(37, 35);
+            this.btnFind.TabIndex = 6;
+            this.btnFind.Text = "》";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(410, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 24);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "结果导出";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridViewout
+            // 
+            this.dataGridViewout.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewout.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.JCartype,
+            this.Column1,
+            this.Column2,
+            this.forcetime,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8});
+            this.dataGridViewout.Location = new System.Drawing.Point(410, 68);
+            this.dataGridViewout.Name = "dataGridViewout";
+            this.dataGridViewout.RowTemplate.Height = 23;
+            this.dataGridViewout.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewout.Size = new System.Drawing.Size(405, 275);
+            this.dataGridViewout.TabIndex = 8;
+            // 
+            // JCartype
+            // 
+            this.JCartype.HeaderText = "JCartype";
+            this.JCartype.Name = "JCartype";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "CarColor";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "PlateNo";
+            this.Column2.Name = "Column2";
+            // 
+            // forcetime
+            // 
+            this.forcetime.HeaderText = "forcetime";
+            this.forcetime.Name = "forcetime";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "imagepath";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "carlenth";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "carwide";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "updateflage";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "CarNo";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "InputCount";
+            this.Column8.Name = "Column8";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 382);
+            this.Controls.Add(this.dataGridViewout);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewout)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridViewout;
+        private System.Windows.Forms.DataGridViewTextBoxColumn JCartype;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn forcetime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Data.OleDb;
+
+namespace Findpic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s_D1 = textBox1.Text;
+            string s_D2 = Regex.Replace(s_D1, @"\d{2}:\d{2}:\d{2}:\d{3}", "");
+            string s_Dout = Regex.Replace(s_D1, @":\d{3}", "");
+            s_Dout = Regex.Replace(s_Dout, "-", "");
+            s_Dout = Regex.Replace(s_Dout, ":", "");
+            s_Dout = Regex.Replace(s_Dout, " ", "");
+            //FindAllPic(s_D2, s_Dout);
+
+        }
+        public int coun = 0;
+        public void FindAllPic(string s_Data, string MarchStr, int CarNo,string forceTime,int Count)
+        {
+
+            string s_OPath = "E:\\HWCarm\\192.168.0.168\\" +s_Data;
+            DirectoryInfo did = new DirectoryInfo(s_OPath);
+            FileInfo[] fissd = did.GetFiles("*.jpg", SearchOption.AllDirectories);
+            //先备份出来
+            foreach (FileInfo fiid in fissd)
+            {
+                if (Regex.IsMatch(fiid.Name, MarchStr))
+                {
+                    File.Copy(s_OPath + "\\" + fiid.Name, "E:\\HWCarm\\LOSTALL\\" + fiid.Name);
+                }
+
+            }
+            string s_Path = "";
+            //s_Path = "E:\\HWCarm\\192.168.0.168\\" + s_Data;
+            s_Path = "E:\\HWCarm\\LOSTALL\\";
+            DirectoryInfo di = new DirectoryInfo(s_Path);
+            FileInfo[] fiss = di.GetFiles("*-s.jpg", SearchOption.AllDirectories);
+            string F1Name = "E:\\HWCarm\\BAK\\";
+            string F2Name = "E:\\HWCarm\\LOST\\";
+            if (!Directory.Exists(F1Name))
+            {
+                Directory.CreateDirectory(F1Name);
+            }
+            if (!Directory.Exists(F2Name))
+            {
+                Directory.CreateDirectory(F2Name);
+            }
+            foreach (FileInfo fii in fiss)
+            {
+                if (Regex.IsMatch(fii.Name, MarchStr))
+                {
+                    File.Move(s_Path + "\\" + fii.Name, "E:\\HWCarm\\BAK\\" + fii.Name);
+                    //WriteLog(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "找到车牌图片：" + fii.Name+"\r\n");
+                }
+
+            }
+            FileInfo[] fis = di.GetFiles("*.jpg", SearchOption.AllDirectories);
+            foreach (FileInfo fi in fis)
+            {
+                if (Regex.IsMatch(fi.Name, MarchStr))
+                {
+                    string CarNum = fi.Name.Substring(fi.Name.Length - 12);
+                    CarNum = Regex.Replace(CarNum, @".jpg", "");
+                    string CarColor = CarNum.Substring(0, 1);
+                    CarNum = CarNum.Remove(0, 1);
+                    //MessageBox.Show(CarNum);
+                    WriteLog(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "找到抓拍图片：" + fi.Name + "\r\n");
+                    coun = dataGridViewout.Rows.Add();
+                    CarNo++;
+                    //MessageBox.Show(fi.Name);
+                    if (CarColor == "蓝")
+                    {
+                        dataGridViewout.Rows[coun].Cells[0].Value = "客1";
+                        dataGridViewout.Rows[coun].Cells[5].Value = 4911;
+                        dataGridViewout.Rows[coun].Cells[6].Value = 1512;
+                    }
+                    else
+                    {
+                        dataGridViewout.Rows[coun].Cells[0].Value = "客3";
+                        dataGridViewout.Rows[coun].Cells[5].Value = 9088;
+                        dataGridViewout.Rows[coun].Cells[6].Value = 3739;
+                    }
+                    dataGridViewout.Rows[coun].Cells[1].Value = CarColor;
+                    dataGridViewout.Rows[coun].Cells[2].Value = CarNum;
+                    dataGridViewout.Rows[coun].Cells[3].Value = forceTime;
+                    dataGridViewout.Rows[coun].Cells[4].Value = "E:\\HWCarm\\192.168.0.168\\" + s_Data+"\\"+ fi.Name;
+                    
+                    dataGridViewout.Rows[coun].Cells[7].Value = "改";
+                    dataGridViewout.Rows[coun].Cells[8].Value = CarNo;
+                    dataGridViewout.Rows[coun].Cells[9].Value = Count;
+                    File.Copy(s_Path + "\\" + fi.Name, "E:\\HWCarm\\LOST\\" + fi.Name);
+                    WriteLog(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "复制抓拍图片：" + "E:\\HWCarm\\LOST\\" + fi.Name + "\r\n");
+                }
+
+            }
+        }
+        //获取Excel
+        public static DataSet GetExcelData(string str)
+        {
+            string strCon = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + str + ";Extended Properties='Excel 12.0;HDR=YES;IMEX=1;'";
+            OleDbConnection myConn = new OleDbConnection(strCon);
+            string strCom = " SELECT * FROM [车道1$]";
+            myConn.Open();
+            OleDbDataAdapter myCommand = new OleDbDataAdapter(strCom, myConn);
+            DataSet myDataSet = new DataSet();
+            myCommand.Fill(myDataSet, "[车道1$]");
+            myConn.Close();
+            return myDataSet;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog filedialog = new OpenFileDialog();
+            string FileName = "";
+
+            if (filedialog.ShowDialog() == DialogResult.OK)
+            {
+                FileName = filedialog.FileName;
+                //((DataGridViewTextBoxColumn)dataGridView1.Columns[5]).MaxInputLength=1024;
+                dataGridView1.DataSource = GetExcelData(FileName);
+                dataGridView1.DataMember = "[车道1$]";
+
+                for (int count = 0; (count <= (dataGridView1.Rows.Count - 1)); count++)
+                {
+                    dataGridView1.Rows[count].HeaderCell.Value = (count + 1).ToString();
+                }
+            }
+        }
+        public string GetRandFor3()
+        {
+            Random ro = new Random();
+            int iR;
+            int iUp = 999;
+            int iDowm = 100;
+            iR = ro.Next(iDowm, iUp);
+            return iR.ToString();
+        }
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            btnFind.Enabled = false;
+            string CarNo = "";
+            string FTimestring = "";
+            string ShouldNum = "";
+            
+            try
+            {
+                for (int i = 0; i < dataGridView1.RowCount-1; i++)
+                {
+                    int ShijiCount = 0;
+                    FTimestring = dataGridView1.Rows[i].Cells[6].Value.ToString();
+                    CarNo = (this.dataGridView1.Rows[i].Cells[1].Value).ToString();
+                    ShouldNum = (this.dataGridView1.Rows[i].Cells[5].Value).ToString();
+                    if (FTimestring != "")
+                    {
+                        string[] sArry = Regex.Split(FTimestring, "    ", RegexOptions.IgnoreCase);
+                        int Num = Convert.ToInt32(CarNo);
+                        foreach (string ss in sArry)
+                        {
+                            if (ss != null || ss != "")
+                            {
+                                string s_D1 = ss;
+                                int i_Count = 0;
+                                string s_D2 = Regex.Replace(s_D1, @"\d{2}:\d{2}:\d{2}:\d{3}", "");
+                                if (s_D1.Length < 3)
+                                { }
+                                else
+                                {
+                                    string s_Last3 = s_D1.Substring(s_D1.Length - 3);
+                                    i_Count = Convert.ToInt16(s_Last3);
+                                }
+                                
+                                
+
+                                string s_Dout = Regex.Replace(s_D1, @":\d{3}", "");
+                                string s_Rtime = Regex.Replace(s_D1, @":\d{3}", ":"+GetRandFor3());
+                                s_Dout = Regex.Replace(s_Dout, "-", "");
+                                s_Dout = Regex.Replace(s_Dout, ":", "");
+                                s_Dout = Regex.Replace(s_Dout, " ", "");
+                                if (i_Count > 0 && i_Count < 10)
+                                {
+                                    s_Dout = (Convert.ToInt64(s_Dout) - 1).ToString();
+                                }
+                                if (s_D2 == "" || s_Dout == "")
+                                { 
+                                }
+
+                                else
+                                {
+                                    ShijiCount = sArry.Length-1;
+                                    WriteLog(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "找到抓拍文件夹：" + s_D2 + "开始CarNo：" + CarNo + "开始CarNo：" + CarNo + "\r\n");
+                                    FindAllPic(s_D2, s_Dout, Num,s_Rtime,ShijiCount);
+                                    Num++;
+                                }
+                                
+                                
+                            }
+
+                        }
+
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.ToString());
+                WriteLog(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "查找异常：" + ex.ToString() + "\r\n");
+                //btnFind.Enabled = true;
+            }
+            btnFind.Enabled = true;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.FileName = DateTime.Now.ToString("yyyyMMdd HHmm");
+            string PathExcel = "";
+
+            saveFileDialog1.Filter = "Excel files(*.xls)|*.xls";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                PathExcel = saveFileDialog1.FileName;
+                ExcelCreate excre = new ExcelCreate();
+                DataTable dt = new DataTable();
+                dt.Columns.Add("JCarType");
+                dt.Columns.Add("CarColour");
+                dt.Columns.Add("PlateNo");
+                dt.Columns.Add("forcetime");
+                dt.Columns.Add("imagepath");
+                dt.Columns.Add("carlength");
+                dt.Columns.Add("carheight");
+                dt.Columns.Add("update_flag");
+                dt.Columns.Add("CarNo");
+                dt.Columns.Add("InPutCount");
+
+                for (int i = 0; i < dataGridViewout.Rows.Count; i++)
+                {
+                    DataRow dr = dt.NewRow();
+                    dr["JCarType"] = (dataGridViewout.Rows[i].Cells[0].Value);
+                    dr["CarColour"] = (dataGridViewout.Rows[i].Cells[1].Value);
+                    dr["PlateNo"] = (dataGridViewout.Rows[i].Cells[2].Value);
+                    dr["forcetime"] = (dataGridViewout.Rows[i].Cells[3].Value);
+                    dr["imagepath"] = (dataGridViewout.Rows[i].Cells[4].Value);
+                    dr["carlength"] = (dataGridViewout.Rows[i].Cells[5].Value);
+                    dr["carheight"] = (dataGridViewout.Rows[i].Cells[6].Value);
+                    dr["update_flag"] = (dataGridViewout.Rows[i].Cells[7].Value);
+                    dr["CarNo"] = (dataGridViewout.Rows[i].Cells[8].Value);
+                    dr["InPutCount"] = (dataGridViewout.Rows[i].Cells[9].Value);
+                    dt.Rows.Add(dr);
+                }
+
+                string dt12 = DateTime.Now.ToString("yyyyMMddhhmmss");
+
+                excre.OutFileToDisk(dt, "数据表", PathExcel);
+                MessageBox.Show("Execl导出成功，路劲为：" + PathExcel);
+            }
+        }
+
+        //写日志
+        public static void WriteLog(string Msg)
+        {
+            // string sDirPath = ".\\log\\" + DateTime.Now.ToString("yyyy-MM-dd") + "\\";
+            string sDirPath = ".\\log\\";
+            CreateDir(sDirPath);
+            string sFilePath = sDirPath + DateTime.Now.ToString("yyyy-MM-dd") + ".log";
+            try
+            {
+                using (StreamWriter sw = File.AppendText(sFilePath))
+                {
+                    //sw.WriteLine(Msg);
+                    sw.Write(Msg);
+                    sw.Flush();
+                    sw.Dispose();
+                }
+            }
+            catch { }
+        }
+        public static void CreateDir(string dirPath)
+        {
+            if (!Directory.Exists(dirPath))
+            {
+                try
+                {
+                    Directory.CreateDirectory(dirPath);
+                }
+                catch { }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
